740fef5302a2ed74d73d39711e1013dddb3506ef
Jeff Wilson <jeff@imtc.gatech.edu>
2025-09-22 12:52:43 -0400

    removed android-only module, replaced by KMP port

--- msak-android/src/main/java/edu/gatech/cc/cellwatch/msak/latency/LatencyResult.kt
+++
@@ -1,9 +0,0 @@
-package edu.gatech.cc.cellwatch.msak.latency
-
-data class LatencyResult(
-    val ID: String,
-    val StartTime: String,
-    val RoundTrips: List<LatencyRoundTrip>,
-    val PacketsSent: Int,
-    val PacketsReceived: Int,
-)
--- msak-android/src/main/java/edu/gatech/cc/cellwatch/msak/throughput/ThroughputDirection.kt
+++
@@ -1,6 +0,0 @@
-package edu.gatech.cc.cellwatch.msak.throughput
-
-/**
- * The direction of a throughput test, from the perspective of this client.
- */
-enum class ThroughputDirection { UPLOAD, DOWNLOAD }
